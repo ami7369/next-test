6e9d1556f5c985a0d6a1209012fbfd5361b6202d
ami7369 <ami_tsumagari@itp.co.jp>
2024-11-11 08:21:19 +0900

    img check

--- testnext/app/page.js
+++ testnext/app/page.js
@@ -6,9 +6,7 @@ const endpoint = `https://dev.gokakunin.net/marine2/api/marine_blog`;
 
 export default async function Home() {
   const time = new Date().toLocaleString();
-  const data = await fetch(endpoint, { next: { revalidate } }).then((res) =>
-    res.json()
-  );
+  const data = await fetch(endpoint, { next: { revalidate } }).then((res)=>res.json());
   return (
     <main className={styles.main}>
       <h1>Render@ {time}</h1>
@@ -21,6 +19,9 @@ export default async function Home() {
           );
         })}
       </ul>
+      <p>
+        <img src="https://images.microcms-assets.io/assets/8f40449166414243988f44799f5e4770/e655d115af744f86a6f4c8416d67ac37/fe4f37afa8fc6e97ebabe2054ae3d3c8_892b7cb679b9e76586d439c27ba15e11.jpg" />
+      </p>
     </main>
   );
 }
